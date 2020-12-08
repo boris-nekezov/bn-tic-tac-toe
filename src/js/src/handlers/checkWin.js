6792b263dfaaae957200f7f8c69adc1bafd39efd
Boris K. Nekezov <boris.nekezov@gmail.com>
2020-12-08 15:07:04 +0200

    Adjust the line that goes through button fot every type of win and style it

--- src/js/src/handlers/checkWin.js
+++ src/js/src/handlers/checkWin.js
@@ -1,6 +1,5 @@
 const checkWin = (matrix, btnContent) => {
 	console.log(`[CHECK WIN]`);
-	// console.log(matrix, btnContent);
 
 	// row wins
 	const isRow1Win =
@@ -23,19 +22,19 @@ const checkWin = (matrix, btnContent) => {
 			: false;
 
 	// column wins
-	const isCow1Win =
+	const isCol1Win =
 		matrix[0][0] == btnContent &&
 		matrix[1][0] == btnContent &&
 		matrix[2][0] == btnContent
 			? true
 			: false;
-	const isCow2Win =
+	const isCol2Win =
 		matrix[0][1] == btnContent &&
 		matrix[1][1] == btnContent &&
 		matrix[2][1] == btnContent
 			? true
 			: false;
-	const isCow3Win =
+	const isCol3Win =
 		matrix[0][2] == btnContent &&
 		matrix[1][2] == btnContent &&
 		matrix[2][2] == btnContent
@@ -56,40 +55,73 @@ const checkWin = (matrix, btnContent) => {
 			? true
 			: false;
 
-	if (
-		// isRow1Win ||
-		// isRow2Win ||
-		// isRow3Win ||
-		isCow1Win ||
-		isCow2Win ||
-		isCow3Win ||
-		isDiag1Win ||
-		isDiag2Win
-	) {
-		return true;
-	}
-
 	const winLine = document.querySelector('.tic-tac-toe__win-line');
+
+	// row wins
 	if (isRow1Win) {
-		console.log('[isRow1Win]');
+		console.log('[Row1Win]');
 		winLine.classList.add(
 			'tic-tac-toe__win-line--win-row-1',
 			'tic-tac-toe__win-line--active'
 		);
 		return true;
 	}
-
 	if (isRow2Win) {
+		console.log('[Row2Win]');
 		winLine.classList.add(
 			'tic-tac-toe__win-line--win-row-2',
 			'tic-tac-toe__win-line--active'
 		);
 		return true;
 	}
-
 	if (isRow3Win) {
+		console.log('[Row3Win]');
 		winLine.classList.add(
 			'tic-tac-toe__win-line--win-row-3',
+			'tic-tac-toe__win-line--active'
+		);
+		return true;
+	}
+
+	// column wins
+	if (isCol1Win) {
+		console.log('[Col1Win]');
+		winLine.classList.add(
+			'tic-tac-toe__win-line--win-col-1',
+			'tic-tac-toe__win-line--active'
+		);
+		return true;
+	}
+	if (isCol2Win) {
+		console.log('[Col2Win]');
+		winLine.classList.add(
+			'tic-tac-toe__win-line--win-col-2',
+			'tic-tac-toe__win-line--active'
+		);
+		return true;
+	}
+	if (isCol3Win) {
+		console.log('[Col3Win]');
+		winLine.classList.add(
+			'tic-tac-toe__win-line--win-col-3',
+			'tic-tac-toe__win-line--active'
+		);
+		return true;
+	}
+
+	//diagonal wins
+	if (isDiag1Win) {
+		console.log('[Diag1Win]');
+		winLine.classList.add(
+			'tic-tac-toe__win-line--win-diag-1',
+			'tic-tac-toe__win-line--active'
+		);
+		return true;
+	}
+	if (isDiag2Win) {
+		console.log('[Diag2Win]');
+		winLine.classList.add(
+			'tic-tac-toe__win-line--win-diag-2',
 			'tic-tac-toe__win-line--active'
 		);
 		return true;
